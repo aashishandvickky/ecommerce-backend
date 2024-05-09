296735d211ac7928e7a3019319869d3e97ece1b2
Ashish Sharma <ashishandvicky@yahoo.com>
2024-05-10 00:03:45 +0530

    get and checkout

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // app.js
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const productRoutes = require('./routes/productRoutes');
 const checkoutRoutes = require('./routes/checkoutRoutes');
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+  };
+
+app.options('*', cors(corsOptions));
 
 // Middleware
 app.use(express.json());
